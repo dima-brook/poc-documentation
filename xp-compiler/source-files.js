f9fc65c4d28d0dcc2a926f0becd81cfbf0a1b37a
VKint <kint@xp.network>
2021-05-11 17:49:44 +0530

    initial index

---
+++ xp-compiler/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["xp_compiler"] = {"name":"","dirs":[{"name":"consts","dirs":[{"name":"diem","files":["mod.rs","xdx.rs"]}],"files":["calls.rs","langs.rs","mod.rs"]}],"files":["deserialize.rs","errors.rs","lib.rs"]};
+sourcesIndex["xp_compiler_test"] = {"name":"","files":["xp-compiler-test.rs"]};
+createSourceSidebar();
