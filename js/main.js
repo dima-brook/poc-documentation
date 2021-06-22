a31507e71ccfbd1e00f0d30aa788bd87b367de8f
VKint <dima@xp.network>
2021-06-22 20:06:59 +0530

    add migration-validator typedocs

--- js/main.js
+++ js/main.js
@@ -9,15 +9,20 @@
         const docTemp = Template('doc-item');
         const ctx = [
 			docElem({
+				"doc": "freezer",
+				"name": "freezer",
+				"desc": "XP-Elrond Migration freezer smart contract"
+			}),
+			docElem({
 				"doc": "elrond_mint_contract",
 				"name": "Elrond Mint Contract",
 				"desc": "XP-Elrond Migration minter smart contract"
 			}),
-            docElem({
-				"doc": "freezer",
-				"name": "freezer",
-				"desc": "XP-Elrond Migration freezer smart contract"
-			})
+			{
+				"href": "migration-validator",
+				"name": "Migration Validator",
+				"desc": "Chain Agnostic bridge interface"
+			}
         ]
         docTemp.bind(ctx)
     })();
